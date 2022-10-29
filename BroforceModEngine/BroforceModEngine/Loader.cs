b2190925bb1b5bf20c0bced4f8b0ebf6e98e1bca
Gorzon38 <64686176+Gorzon38@users.noreply.github.com>
2022-10-29 17:53:21 +0200

    A lot of dependencies, don't know why that much

--- BroforceModEngine/BroforceModEngine/Loader.cs
+++ BroforceModEngine/BroforceModEngine/Loader.cs
@@ -5,9 +5,24 @@ namespace BroforceModEngine
 {
     public static class Loader
     {
+        public static string EngineDirectoryPath
+        {
+            get 
+            {
+                return Path.Combine(Directory.GetCurrentDirectory(), "BroforceModEngine");
+            }
+        }
+
         public static void Main()
         {
-
+            if(Directory.Exists(EngineDirectoryPath))
+            {
+                //
+            }
+            else
+            {
+                Directory.CreateDirectory(EngineDirectoryPath);
+            }
         }
     }
 }
