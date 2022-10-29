604394bb2271bb982e3358f5efdb8e81481002d6
Loki hornsby <lokihornsby@gmail.com>
2022-10-29 11:36:27 +0100

    Setup
    Im still tryna learn shit so there's nothing here (yet!)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+// .Net needs to be 3.5
+// Path aiming for is "~/BloodParticle.cs"
+
+namespace BroMods {
+    class Program {
+        static void Main(string[] args){
+            
+        }
+    }
+}
